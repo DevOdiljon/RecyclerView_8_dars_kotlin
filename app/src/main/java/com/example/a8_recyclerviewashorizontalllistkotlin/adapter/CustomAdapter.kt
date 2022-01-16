ee7df948492abcb62bdb0da75e02456133949a1e
HP <rodilbek81@gmail.com>
2022-01-16 21:11:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a8_recyclerviewashorizontalllistkotlin/adapter/CustomAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.a8_recyclerviewashorizontalllistkotlin.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a8_recyclerviewashorizontalllistkotlin.R
+import com.example.a8_recyclerviewashorizontalllistkotlin.model.Member
+
+class CustomAdapter(val members: List<Member>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout_view, parent, false)
+        return CustomViewHolder(view)
+    }
+
+    class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+      val member = members.get(position)
+    }
+
+    
+
+    override fun getItemCount(): Int {
+        return members.size
+    }
+
+}
---
+++ app/src/main/java/com/example/a8_recyclerviewashorizontalllistkotlin/model/Member.kt
@@ -0,0 +1,5 @@
+package com.example.a8_recyclerviewashorizontalllistkotlin.model
+
+data class Member(
+    val num:Int
+)
